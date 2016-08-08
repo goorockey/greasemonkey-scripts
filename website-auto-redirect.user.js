1700e928779c90c599b5f5007d91a7cbf9d22c57
goorockey <kelvingu616@gmail.com>
2016-08-08 19:23:12 +0800

    update website auto redirect

--- website-auto-redirect.user.js
+++ website-auto-redirect.user.js
@@ -6,10 +6,11 @@
 // @include     http://wanqu.co/*
 // @exclude     http://wanqu.co/blog/*
 // @include     http://v.videohupu.com/*/v*.html
+// @include     http://v.liangle.com/*/v*.html
 // @include     https://www.producthunt.com/*
 // @downloadURL https://raw.githubusercontent.com/goorockey/greasemonkey-scripts/master/website-auto-redirect.user.js
 // @updateURL   https://raw.githubusercontent.com/goorockey/greasemonkey-scripts/master/website-auto-redirect.user.js
-// @version     1.4
+// @version     1.5
 // @grant       none
 // author       kelvingu616@gmail.com
 // github       github.com/goorockey
@@ -20,6 +21,7 @@ var website_target = {
   'next.36kr.com': '//a[@class="post-url"]/@href',
   'toutiao.io': '//h3[@class="title"]/a[1]/@href',
   'v.videohupu.com': '//a[@class="play_btn"]/@href',
+  'v.liangle.com': '//a[@class="play_btn"]/@href',
   'www.producthunt.com': '//main//a[1]/@href',
 };
 
