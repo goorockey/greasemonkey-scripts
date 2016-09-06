af03dc8deb0ff4563647e1ab2ffb08210899fbd7
goorockey <kelvingu616@gmail.com>
2016-09-06 16:09:01 +0800

    update domain of btbbt-filter

--- btbbt-filter.user.js
+++ btbbt-filter.user.js
@@ -3,9 +3,10 @@
 // @grant       none
 // @description filter movies in btbbt.cc
 // @include     /^http://(www.)*btbbt.cc/forum-.*$/
+// @include     /^http://(www.)*btbtt.la/forum-.*$/
 // @downloadURL https://raw.githubusercontent.com/goorockey/greasemonkey-scripts/master/btbbt-filter.user.js
 // @updateURL   https://raw.githubusercontent.com/goorockey/greasemonkey-scripts/master/btbbt-filter.user.js
-// @version     1.0.1
+// @version     1.0.2
 // author       kelvingu616@gmail.com
 // github       github.com/goorockey
 // @require     http://cdn.staticfile.org/jquery/2.2.1/jquery.min.js
